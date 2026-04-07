b86692d274123c87b0b7ae8e4af564c7b5347e4e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-06 21:42:02 -0300

    Aula 06/04

--- App.tsx
+++ App.tsx
@@ -1,57 +1,6 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-type Consulta = {
-  id: number;
-  paciente: string;
-  medico: string;
-  data: string;
-  status: "agendada" | "confirmada" | "cancelada" | "realizada";
-};
+import React from "react";
+import { Home } from "./src/screens";
+
 export default function App() {
-  const [consulta, setConsulta] = useState<Consulta>({
-    id: 1,
-    paciente: "Carlos Andrade",
-    medico: "Dr. Roberto Silva",
-    data: "28/02/2026",
-    status: "agendada",
-  });
-  function confirmarConsulta() {
-    setConsulta({
-      ...consulta,
-      status: "confirmada",
-    });
-  }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Sistema de Consultas</Text>
-      <View style={styles.card}>
-        <Text>Paciente: {consulta.paciente}</Text>
-        <Text>Médico: {consulta.medico}</Text>
-        <Text>Data: {consulta.data}</Text>
-        <Text>Status: {consulta.status}</Text>
-        {consulta.status === "agendada" && (
-          <Button title="Confirmar Consulta" onPress={confirmarConsulta} />
-        )}
-      </View>
-    </View>
-  );
+  return <Home />;
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  titulo: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  card: {
-    width: "80%",
-    padding: 20,
-    borderWidth: 1,
-    borderRadius: 8,
-  },
-});
--- src/interfaces/medico.ts
+++ src/interfaces/medico.ts
@@ -1,4 +1,4 @@
-import { Especialidade } from "../types/especialdiade";
+import { Especialidade } from "../types/especialidade";
 export interface Medico {
     id: number;
     nome: string;
